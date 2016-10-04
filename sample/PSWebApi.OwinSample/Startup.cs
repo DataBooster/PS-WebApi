eaf3e7a2f45a8c14f41a6a4834eaac712e86351c
Abel Cheng <abelcys@gmail.com>
2016-10-03 23:18:02 -0400

    Preparing empty PSWebApi.OwinSample project

---
+++ sample/PSWebApi.OwinSample/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(PSWebApi.OwinSample.Startup))]
+
+namespace PSWebApi.OwinSample
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+		}
+	}
+}
